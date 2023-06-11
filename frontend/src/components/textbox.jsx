4ec921049406bad7746a0f30c65884739f1a9c36
Anurag-Raut <rautab0@gmail.com>
2023-06-11 16:23:50 +0530

    fixed bug

--- frontend/src/components/textbox.jsx
+++ frontend/src/components/textbox.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:4000');
+const socket = io('https://cdocsocket.onrender.com');
 
 const debounce = (func, delay) => {
   let timeoutId;
